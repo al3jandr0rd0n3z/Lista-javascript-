268403a65af06e513a939a2f93fbc159efbc293c
alejandro:) <115997540+al3jandr0rd0n3z@users.noreply.github.com>
2023-01-12 14:46:59 +0100

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -1,8 +1,52 @@
-alert("cargado");
-function total_lista(lista){
-    return lista.lenght;
+for(var i=0; i <=10; i++){
+    console.log(i);
 }
 
-function suma(x,y){
-    return x+y;
+var frutasyverduras = ["pera", "mango","avocado"]
+
+frutasyverduras.push("fresa");
+
+frutasyverduras[2]= "limon"
+
+for(var i=0; i<frutasyverduras; i++){
+    console.log(frutasyverduras[i]);
+}
+var j=0
+while(j< frutasyverduras.length){
+    console.log(frutasyverduras[j]);
+    j=j+1;
+}
+
+function totallista(lista){
+    return lista.length;
+}
+
+function esonoes(lista,elemento){
+    for(var i=0; i<lista.length; i++){
+        if(lista[i] == elemento){
+            return true;
+        }
+    }
+    return false;
+}
+
+function elimina(lista, elemento){
+    for(var i=0; i<lista.length; i++){
+        if(lista[i]== elemento){
+            
+        }
+    }
+                                      
+}
+
+
+function mayor(lista){
+     var mayor = lista[0];
+
+     for(var i=0; i<lista.length; i++){
+        if(mayor<lista[i]){
+            mayor = lista [i];
+        }
+     }
+     return mayor;
 }
